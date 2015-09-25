07568fcbcc1478cd84fab2b5d31c539b8d26404b
rachadrienne <rachelmalone@Rachels-MBP.home.gateway>
2015-09-25 19:30:10 +1000

    All cities are included

--- js/main.js
+++ js/main.js
@@ -4,18 +4,18 @@
 
     $(document).ready(function () {
 
-                //1 User inputs city
-                // 1.1 if user inputs New York, New York City or NYC
-                // 1.1.1 Then Display nyc.jpg
-                // 1.2 if user inputs San Francisco, SF or Bay Area
-                // 1.2.1 Then Display sf.jpg
-                // 1.3 if user inputs Los Angeles, LA or LAX
-                // 1.3.1 Then Display la.jpg
-                // 1.4 if user inputs Austin or ATX
-                // 1.4.1 Then Display austin.jpg
-                // 1.5 if user inputs Sydney, SYD
-                // 1.5.1 Then Display sydney.jpg
-                // 1.6 any else alert user that city does not have an image
+        //1 User inputs city
+        // 1.1 if user inputs New York, New York City or NYC
+        // 1.1.1 Then Display nyc.jpg
+        // 1.2 if user inputs San Francisco, SF or Bay Area
+        // 1.2.1 Then Display sf.jpg
+        // 1.3 if user inputs Los Angeles, LA or LAX
+        // 1.3.1 Then Display la.jpg
+        // 1.4 if user inputs Austin or ATX
+        // 1.4.1 Then Display austin.jpg
+        // 1.5 if user inputs Sydney, SYD
+        // 1.5.1 Then Display sydney.jpg
+        // 1.6 any else alert user that city does not have an image
 
 
         function changeBackground() {
@@ -23,29 +23,41 @@
             var city = $('#city-type').val();
 
             if ((city === 'NYC') || (city === 'New York') || (city === 'New York City')) {
-
+                // changes background to NYC
                 $('body').css('backgroundImage', 'url(../images/nyc.jpg)');
 
-            } else if ((city === 'Bay Area') || (city === 'San Francisco') || (city === 'San Fran')) {
-
+            } else if ((city === 'bay area') || (city === 'san francisco') || (city === 'sf')) {
+                // changes background to San Fran
                 $('body').css('backgroundImage', 'url(../images/sf.jpg)');
-        
 
+            } else if ((city === 'LA') || (city === 'LAX') || (city === 'Los Angeles')) {
+                // changes background to LA
+                $('body').css('backgroundImage', 'url(../images/la.jpg)');
+
+            } else if ((city === 'Austin') || (city === 'ATX')) {
+
+                // changes background to Austin
+                $('body').css('backgroundImage', 'url(../images/austin.jpg)');
+
+            } else if ((city === 'SYD') || (city === 'Sydney')) {
+
+                // changes background to Sydney
+                $('body').css('backgroundImage', 'url(../images/sydney.jpg)');
+                
             } else {
 
+                // alerts users that that city is not available
                 alert('We don\'t have any images of ' + city);
             }
         }
-
-        $("#submit-btn").click(function (event) {
+        
+        // stops the submit button from firing off a submit request
+        $('#submit-btn').click(function (event) {
             event.preventDefault();
         });
 
+        // click event to make the background change happen
         $('#submit-btn').click(changeBackground);
-
-
-
-
 
     });
 
